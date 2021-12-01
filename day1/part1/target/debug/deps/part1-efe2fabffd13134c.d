68bf97b17df6592d11d53328813b854b1bc7dd17
Jamie Barnett <jamiebarnett1992@gmail.com>
2021-12-01 21:09:57 +0000

    gitignore

--- day1/part1/target/debug/deps/part1-efe2fabffd13134c.d
+++
@@ -1,5 +0,0 @@
-/Users/jamiebarnett/dev/aoc_2021/day1/part1/target/debug/deps/part1-efe2fabffd13134c.rmeta: src/main.rs
-
-/Users/jamiebarnett/dev/aoc_2021/day1/part1/target/debug/deps/part1-efe2fabffd13134c.d: src/main.rs
-
-src/main.rs:
--- day1/part1/target/debug/part1.d
+++
@@ -1,1 +0,0 @@
-/Users/jamiebarnett/dev/aoc_2021/day1/part1/target/debug/part1: /Users/jamiebarnett/dev/aoc_2021/day1/part1/src/main.rs
